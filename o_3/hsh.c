5b3896f431554807c2f422b2a19a1c779b3b090c
AsiaEasa <asiaeasa89@gmail.com>
2023-11-04 06:27:32 -0700

    done

---
+++ o_3/hsh.c
@@ -0,0 +1,35 @@
+#include "hsh.h"
+
+/**
+ * main - function that checks if our shell is called
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char *line;
+	char **arg;
+	int stat, x;
+
+	stat = -1;
+	x = 1;
+
+	do { if (isatty(STDIN_FILENO) == 0)
+			x = 0;
+			else
+		_puts("$ ");
+
+		line = get_line(); /* read line from stdin */
+		arg = split(line); /* tokenize line */
+		stat = exec_arg(arg);
+		/* avoid memory leaks */
+		free(line);
+		free(arg);
+		/* exit with status */
+		if (stat >= 0)
+		{
+			exit(stat);
+		}
+	} while (stat == -1 && x);
+	return (0);
+}
